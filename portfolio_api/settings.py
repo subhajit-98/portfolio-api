c4241e933093bf3bb17fd71d7a8eafb09732f91e
subhajit-98 <subhajitdey.1998@gmail.com>
2021-07-11 02:34:07 +0530

    App setup done

--- portfolio_api/settings.py
+++ portfolio_api/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'about',
+    'contact',
+    'education',
+    'experince',
+    'projects'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+API_ROOT = '/api/v1/'
