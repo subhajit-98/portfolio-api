c111749cdec15602475565f05c8ea72b38d6ce53
subhajit-98 <subhajitdey.1998@gmail.com>
2021-08-02 17:28:20 +0530

    exception handleing on smtp mail sent

--- email_queue/views.py
+++ email_queue/views.py
@@ -4,15 +4,19 @@ from email_queue.models import EmailQueue
 # Create your views here.
 
 def email_send_corn_job():
-    email_queue = EmailQueue.objects.filter(email_status="0")[:1]
-    for email_id_list in email_queue:
-        email = EmailMessage(email_id_list.email_subject, 'Body', to=[email_id_list.email_to])
-        resp = email.send()
+    try:
+        email_queue = EmailQueue.objects.filter(email_status="0")
+        for email_id_list in email_queue:
+            email = EmailMessage(email_id_list.email_subject, 'Body', to=[email_id_list.email_to])
+            resp = email.send()
+            print (resp)
 
-        if resp == 1:
-            update_email_queue = EmailQueue.objects.get(id=email_id_list.id)
-            update_email_queue.email_status = "1"
-            update_email_queue.save()
+            if resp == 1:
+                update_email_queue = EmailQueue.objects.get(id=email_id_list.id)
+                update_email_queue.email_status = "1"
+                update_email_queue.save()
+    except Exception as e:
+        print (e)
 
 
 # python manage.py shell --command="from email_queue.views import email_send; email_send()"
